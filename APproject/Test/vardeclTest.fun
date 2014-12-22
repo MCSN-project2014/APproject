ae78909d968e16e2ce278809e4113118c96c6518
dido <davide.neri@stud.unifi.it>
2014-12-22 18:51:41 +0100

    A first structure of Abstract Syntax Tree (AST)
    I have added two classes:
    -Node,
    -TreeGen .

--- APproject/Test/vardeclTest.fun
+++ APproject/Test/vardeclTest.fun
@@ -1,5 +1,17 @@
+
+fun add ( a int , b int){
+	var z int = a + b;
+}
+
+fun sub ( a int, b int )  {
+	var s int = a - b ; 
+}
+
 fun main(){
 	var b bool;			//declaration
 	var c, d fun;		//two declarations
 	var a int = 5+3;    //declaration and assignment
+
+
 }
+
---
+++ APproject/treeGenerator/TreeGen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APproject.treeGenerator
+{
+    class TreeGen
+    {
+        public Node root;
+
+        public TreeGen(){}
+    
+
+        public Node createExp( String n, Types rtype, String etype){ return new Exp(n,rtype,etype);}
+        public Node createIf(Exp e, Stmt s) {return new If( e, s );}
+        public Node createElse(Exp e, Stmt s1, Stmt s2){return new Else( e, s1, s2);}
+        public Node createFor(Exp eStart, Exp eMiddle, Exp eEnd, Stmt s) { return new For(eStart, eMiddle, eEnd, s); }
+        public Node createDecl(String id, Exp expd, String dType){return new Decl(id, expd,  dType);}
+        }
+    }
+}
