25376a7c4d33ebfec2cce5593fcdf30831e841f2
stefano <13201749@ucdconnect.ie>
2014-12-24 12:29:39 +0100

    Comments

--- APproject/FSCodeGenerator/FSCodeGen.cs
+++ APproject/FSCodeGenerator/FSCodeGen.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace APproject.FSCodeGenerator
 {
+    /**
+     * This class converts an AST of funW@P into the corresponding F#
+     * code, simply by visiting the tree.
+     * Output is written into a .fs file, result of the translation.
+     * */
+
     class FSCodeGen
     {
+        public FSCodeGen()
+        {
+
+        }
     }
 }
