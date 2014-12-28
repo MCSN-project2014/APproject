3426c2214549fc54fbb4f98c844dedf7beb5acd9
lucarin91 <lucarin91@gmail.com>
2014-12-28 15:03:13 +0100

    add to the build the files FSCodeGen

--- APproject/Interpreter/Interpreter.cs
+++ APproject/Interpreter/Interpreter.cs
@@ -14,7 +14,7 @@ namespace APproject
 		}
 
 		public static void tryInterpreter(){
-			ASTNode If = test1 ();
+			ASTNode If = test3 ();
 			printAST (If);
 			Interpreter inter = new Interpreter (If);
 			inter.Start ();
@@ -123,6 +123,12 @@ namespace APproject
 			}
 		}
 
+		/// <summary>
+		/// if 3>=1 {
+		/// 	var a int= 3 + 7;
+		/// 	println (a);
+		/// }
+		/// </summary>
 		public static ASTNode test1 (){
 			//Obj o = new Obj ();
 			//o.kind = Kinds.func;
@@ -155,6 +161,103 @@ namespace APproject
 			return If;
 		}
 
+		/// <summary>
+		/// if 3>=1 {
+		/// 	var a int= 3 + 7;
+		/// }
+		/// var a int = 3;
+		/// println (a);
+		/// </summary>
+		public static ASTNode test2 (){
+			Node blockM = new Node (Labels.Block);
+
+			Obj varA = new Obj();
+			varA.name = "a";
+
+			Node If = new Node ( Labels.If);
+			blockM.addChildren (If);
+			Node condition = new Node (Labels.Gte);
+			If.addChildren (condition);
+			Term term1 = new Term(3);
+			Term term2 = new Term(1);
+			condition.addChildren (term1);
+			condition.addChildren (term2);
+			Node block = new Node (Labels.Block);
+			If.addChildren (block);
+			Node decAss2 = new Node (Labels.AssigDecl);
+			block.addChildren (decAss2);
+			Term num1 = new Term (3);
+			Term num2 = new Term (7);
+			Node plus = new Node (Labels.Plus);
+			plus.addChildren (num1);
+			plus.addChildren (num2);
+			decAss2.addChildren (new Term (varA));
+			decAss2.addChildren (plus);
+
+			Node decAss1 = new Node (Labels.AssigDecl);
+			blockM.addChildren (decAss1);
+			num1 = new Term (3);
+			decAss1.addChildren (new Term (varA));
+			decAss1.addChildren (num1);
+
+			Node print = new Node (Labels.Print);
+			blockM.addChildren (print);
+			print.addChildren (new Term (varA));
+
+			return blockM;
+		}
+
+		/// <summary>
+		/// var a int = 3;
+		/// if 3>=1 {
+		/// 	a = 3 + 7;
+		/// }
+		/// println (a);
+		/// </summary>
+		public static ASTNode test3 (){
+			/*
+			var a int = 3;
+			if 3>=1 {
+				a = 3 + 7;
+			}
+			println (a);
+			*/
+			Node blockM = new Node (Labels.Block);
+
+			Node decAss1 = new Node (Labels.AssigDecl);
+			blockM.addChildren (decAss1);
+			Obj varA = new Obj();
+			varA.name = "a";
+			Term num1 = new Term (3);
+			decAss1.addChildren (new Term (varA));
+			decAss1.addChildren (num1);
+
+			Node If = new Node (Labels.If);
+			blockM.addChildren (If);
+			Node condition = new Node (Labels.Gte);
+			If.addChildren (condition);
+			Term term1 = new Term(3);
+			Term term2 = new Term(1);
+			condition.addChildren (term1);
+			condition.addChildren (term2);
+			Node block = new Node (Labels.Block);
+			If.addChildren (block);
+			Node decAss2 = new Node (Labels.AssigDecl);
+			block.addChildren (decAss2);
+			num1 = new Term (3);
+			Term num2 = new Term (7);
+			Node plus = new Node (Labels.Plus);
+			plus.addChildren (num1);
+			plus.addChildren (num2);
+			decAss2.addChildren (new Term (varA));
+			decAss2.addChildren (plus);
+
+			Node print = new Node (Labels.Print);
+			blockM.addChildren (print);
+			print.addChildren (new Term (varA));
+
+			return blockM;
+		}
 		public static void printAST(ASTNode node){
 			if (node != null){
 				if (node.isTerminal())
