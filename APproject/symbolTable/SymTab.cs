b70aca55b9f70d18a2dfef2cd0650c39c0df2c38
Mene90 <menelaosralli@gmail.com>
2014-12-28 15:48:20 +0100

    Grammar complete
    with:
    -enviroment for each new block (i.e if while fun)
    -type checking of exp and formal parameters of functions
    missing
    -type checking of return type
    -controll of println and readln belonging to functions

--- APproject/symbolTable/SymTab.cs
+++ APproject/symbolTable/SymTab.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace APproject
 {
 	public enum Types {undef,integer,boolean,fun}
-	public enum Kinds {var,proc,func,form,act,scope}
+	public enum Kinds {var,proc,act,scope}
 
 	public class Obj {  // object describing a declared name
 		public string name;		// name of the object
@@ -15,17 +15,10 @@ namespace APproject
 		public int level;			// nesting level; 0=global, 1=local
 		public Obj locals;		// scopes: to locally declared objects
 		public int nextAdr;		// scopes: next free address in this scope
-	    public Queue<Obj> formal { get; set; }
+	    public Queue<Obj> formals { get; set; }
 	}
 
 	public class SymbolTable {
-
-		/*const int // types
-			undef = 0, integer = 1, boolean = 2, fun = 3;
-		*/
-		/*const int // object kinds
-	        var = 0, proc = 1, func = 2, form = 3, act = 4, scope = 5;
-		*/
 
 		public int curLevel;	// nesting level of current scope
 		public Obj undefObj;	// object node for erroneous symbols
@@ -36,32 +29,39 @@ namespace APproject
 		public SymbolTable(Parser parser) {
 			this.parser = parser;
 			topScope = null;
-			curLevel = -1;
+
 			undefObj = new Obj();
 			undefObj.name  =  "undef"; undefObj.type = Types.undef; undefObj.kind = Kinds.var;
 			undefObj.adr = 0; undefObj.level = 0; undefObj.next = null;
 		}
 
-
-		// open a new scope and make it the current scope (topScope)
-		public void OpenScope () {
+        ///<summary>
+		/// open a new scope and make it the current scope (topScope)
+		/// <summary>
+        public void OpenScope () {
 			Obj scop = new Obj();
-	        Queue<Obj> formali = new Queue<Obj>();
-			scop.name = ""; scop.kind = Kinds.scope;
-	        scop.locals = null; scop.formal = formali; 
+			scop.name = ""; 
+            scop.kind = Kinds.scope;
+	        scop.locals = null; 
 	        scop.nextAdr = 0;
-			scop.next = topScope; topScope = scop; 
+			scop.next = topScope; 
+            topScope = scop; 
 			curLevel++;
 		}
 
-
-		// close the current scope
+        ///<summary>
+		/// close the current scope
+        /// <summary>
 		public void CloseScope () {
 			topScope = topScope.next; curLevel--;
 		}
 
-
-		// create a new object node in the current scope
+        /// <summary>
+		/// create a new object node in the current scope
+        /// <summary>
+        /// <param name="name">name.</param>
+        /// <param name="kind">kind.</param>
+        /// <param name="type">type.</param>
 		public Obj NewObj (string name, Kinds kind, Types type) {
 	        Obj p, last, obj = new Obj(); 
 			obj.name = name; obj.kind = kind; obj.type = type;
@@ -73,20 +73,34 @@ namespace APproject
 			}
 			if (last == null) topScope.locals = obj; else last.next = obj;
 			if (kind == Kinds.var) obj.adr = topScope.nextAdr++;
-	     
-	        if (kind == Kinds.form)
-	        {
-	            obj.adr = topScope.nextAdr++;
-	            topScope.formal.Enqueue(obj);
-	        }
+            if (kind == Kinds.proc) obj.formals = new Queue<Obj>();
+	       
 			return obj;
 		}
 
-	    // set formal paramiters 
-	    public void setformsof(Obj obj)
-	    {
-	        obj.formal = topScope.formal;
-	    }
+        /// <summary>
+        /// Add a formal formal to procedure
+        /// <summary>
+        /// <param name="procedure">procedure.</param>
+        /// <param name="parameter">parameter.</param>
+
+        public void addFormal(Obj procedure, Obj parameter)
+        {
+            procedure.formals.Enqueue(parameter);
+        }
+
+        /// <summary>
+        /// Set the return type of the procedure
+        /// <summary>
+        /// <param name="procedure">procedure.</param>
+        /// <param name="type">type.</param>
+        
+        public void setRType(Obj procedure, Types type)
+        {
+            procedure.type = type;
+        }
+
+	
 
 		/// <summary>
 		/// Checks that the actual parameter type of a function call have the same type of the formal parameters.
@@ -95,12 +109,11 @@ namespace APproject
 		/// <param name="actualTypes">Actual types.</param>
 	    public void checkActualFormalTypes(Obj obj, Queue<Types> actualTypes)
 	    {
-	        if (obj.formal.Count != actualTypes.Count)
+	        if (obj.formals.Count != actualTypes.Count)
 	            parser.SemErr("parameter expected");
 	        else
 	        {
-	            // Copies the entire source Queue to a new standard array.
-	            Obj[] ArrayofFormals = obj.formal.ToArray();
+	            Obj[] ArrayofFormals = obj.formals.ToArray();
 	            for (int i = 0; i < ArrayofFormals.Length; i++)
 	            {
 	                Obj formal = ArrayofFormals[i];
@@ -108,23 +121,19 @@ namespace APproject
 	                if (formal.type != actType)
 	                {
 						parser.SemErr(formal.type + "type expected");
-						/*
-	                    if (formal.type == Types.boolean)
-	                        parser.SemErr("boolean type expected");
-						if (formal.type == Types.iteger)
-	                        parser.SemErr("integer type expected");
-						if (formal.type == Types.fun)
-	                        parser.SemErr("fun type expected");
-	                    */
 	                }
 	            }
 
 	        }
 	    }
 
-		// search the name in all open scopes and return its object node
+        /// <summary>
+        /// search the name in all open scopes and return its object node
+        /// </summary>
+        /// <param name="name">name.</param>
+
 		public Obj Find (string name) {
-			Obj obj, scope;
+            Obj obj, scope; 
 			scope = topScope;
 			while (scope != null) {  // for all open scopes
 				obj = scope.locals;
