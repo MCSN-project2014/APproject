088c3e4da6fc6e6e4ab083f08fd55af896f4515d
lucarin91 <lucarin91@gmail.com>
2015-01-02 20:52:50 +0100

    some test

--- funwaps/FunwapsMain.cs
+++ funwaps/FunwapsMain.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using APproject;
+using System.Collections.Generic;
 
 namespace funwaps
 {
@@ -6,7 +10,23 @@ namespace funwaps
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			var setting = new JsonSerializerSettings () {
+				ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+				NullValueHandling = NullValueHandling.Ignore
+			};
+			var json = JsonConvert.SerializeObject(InterpreterTest.testAFun (),setting);
+			Console.WriteLine (json);
+
+			//args = new string[1];
+			//args [0] = json;
+
+			if (args.Length == 1) {
+				dynamic ret = JsonConvert.DeserializeObject<dynamic> (args [0]);
+				var root = TestJson.deserialize (ret);
+				InterpreterTest.printAST (root);
+				new Interpreter (root).Start ();
+			} else
+				Console.WriteLine ("no input");
 		}
 	}
 }
--- funwaps/testJson.cs
+++ funwaps/testJson.cs
@@ -19,25 +19,17 @@ namespace APproject
 			//Console.WriteLine(json);
 
 			dynamic ret = JsonConvert.DeserializeObject<dynamic>(json);
-			InterpreterTest.printAST(deserialize (ret, new Dictionary<string, Obj>(), new Dictionary<string, Obj>()));
-			new Interpreter (deserialize (ret, new Dictionary<string, Obj>(), new Dictionary<string, Obj>())).Start ();
+			InterpreterTest.printAST(deserialize (ret));
+			new Interpreter (deserialize (ret)).Start ();
 
 		}
 
 
-		private static ASTNode deserialize(dynamic ret, Dictionary<string,Obj> variables, Dictionary<string,Obj> functions){
+		public static ASTNode deserialize(dynamic ret){
 			if (ret.children == null) {
 				//Console.WriteLine (ret.value.GetType ());
 				if (ret.value is JObject) {
-					Obj value;
-					var namestr = Convert.ToString (ret.value.name);
-					if (variables.TryGetValue (namestr, out value))
-						return new Term (value);
-					else {
-						var tmp = new Obj{ name = namestr };
-						variables.Add (namestr, tmp);
-						return new Term (tmp);
-					}
+					return new Term (new Obj{ name = Convert.ToString(ret.value.name) });
 				} else {
 					if (ret.value.Type == JTokenType.Boolean)
 						return new Term (ret.value.ToObject<bool> ());
@@ -46,21 +38,13 @@ namespace APproject
 				}
 			}else{
 				Node n;
-				if (ret.value != null){ 
-					Obj value;
-					var namestr = Convert.ToString (ret.value.name);
-					if (functions.TryGetValue (namestr, out value))
-						n = new Node ((Labels)ret.label, value);
-					else{
-						var tmp = new Obj{ name = namestr };
-						functions.Add (namestr, tmp);
-						n = new Node ((Labels)ret.label, tmp);
-					}
-				}else
+				if (ret.value != null)
+					n = new Node ((Labels)ret.label, new Obj{name = Convert.ToString (ret.value.name)});
+				else
 					n = new Node ((Labels)ret.label);
 
 				foreach (dynamic p in ret.children)
-					n.addChildren(deserialize (p, variables,functions));
+					n.addChildren(deserialize (p));
 
 				return n;
 			}
