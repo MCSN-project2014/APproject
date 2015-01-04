22f946bdf1415d646b187f47aef34758a1deb0f5
stefano <13201749@ucdconnect.ie>
2015-01-04 15:38:38 +0100

    translated ignored

--- APproject/Test/translated_file.fs
+++ APproject/Test/translated_file.fs
@@ -6,3 +6,20 @@ while a <> 10 do
     
     Console.WriteLine(a)
     a <- a + 1
+open System
+open System.IO
+
+let rec fib n  = 
+    let mutable retVal = 0
+    if n = 0 then
+        retVal <- 1
+    if n = 1 then
+        retVal <- 1
+
+    else
+        retVal <- fib  n - 1  + fib  n - 2 
+    
+    retVal
+let mutable retVal = fib  5 
+
+Console.WriteLine(retVal)
